89e7018736bbd126dfb76eab5aa4d0ad514477a8
bryantto08 <bryantto08@gmail.com>
2022-11-05 16:41:29 -0400

    fixed scraping error

--- main.py
+++ main.py
@@ -2,29 +2,45 @@ from selenium import webdriver
 import requests
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+# Initiates webdriver
 
-# Initiate webdriver
-driver = webdriver.Chrome(executable_path='chromedriver.exe')
+
+
+def init_driver(type):
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    driver = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
+    if type == "player_stats":
+        driver.get("https://www.espn.com/nfl/stats/player")
+    else:
+        driver.get("https://www.espn.com/")
+    driver.implicitly_wait(10)  # Waiting for page to load
+    return driver
 
 
 def team_leader(team):
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    driver = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
     driver.get("https://www.espn.com/")
-    driver.maximize_window()
     driver.implicitly_wait(10)  # Waiting for page to load
 
     #  Clicking NFL tab first, then specific team, then by stats page
     driver.find_element(By.XPATH, "/html/body/div[5]/div[2]/header/nav[1]/ul/li[1]/a/span/span[1]").click()
-    driver.find_element(By.XPATH, f"//span[contains(text(),'{team}')]").click()
+    driver.find_element(By.PARTIAL_LINK_TEXT, team.split()[-1]).click()
     driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/main/div[2]/div[2]/nav/ul/li[3]/a/span").click()
 
+    # Using BeautifulSoup to Parse HTML
     r = requests.get(driver.current_url).content
     doc = BeautifulSoup(r,  "html.parser")
 
-    # Getting Team Leader Stats
+    # Getting Team Leader Stats via BeautifulSoup
     tag = doc.find("section", class_="StatLeaders flex")
     team_leader = tag.children
-    players = []
-    for i in team_leader:
+    players = []  # Storing Players in List
+    for i in team_leader:  # Each Player is a Dictionary
         player = {"stat_name": i.find("h2", class_="h8 mb2 clr-gray-03").text,
                   "name": i.find("span", class_="Athlete__PlayerName").text,
                   "position": i.find("span", class_="Athlete__NameDetails ml2 clr-gray-04 di ns9").text,
@@ -34,6 +50,10 @@ def team_leader(team):
         # print(position.text + ": " + name.text + ", " + stat.text)
     return players
 
+def player_stats(player):
+    driver = init_driver("player_stats")
+
+    # Clicking
 
 
 
--- server.py
+++ server.py
@@ -6,11 +6,23 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    return render_template("index.html")
+
+
+@app.route("/team_leaders", methods=["GET", "POST"])
+def team_leaders():
     team_leaders = []
     if request.method == "POST":
         team = request.form["team"]
         team_leaders = main.team_leader(team)
-    return render_template("index.html", team_leaders=team_leaders)
+    return render_template("team_leaders.html", team_leaders=team_leaders)
+
+
+@app.route("/player_stats", methods=["GET", "POST"])
+def player_stats():
+    return
+
+#TODO: News, specific player stats
 
 
 if __name__ == "__main__":
